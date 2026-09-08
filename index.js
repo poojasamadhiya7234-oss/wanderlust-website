d17a683c19521f41011b2d1637524141b8c2c13c
Pooja Samadhiya <poojasamadhiya7234@gmail.com>
2026-09-09 00:39:05 +0530

    Fix deployment configuration

--- index.js
+++ index.js
@@ -1,101 +1,94 @@
-if(process.env.NODE_ENV!="production"){
-  require('dotenv').config()  
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config();
 }
 
+const express = require("express");
+const app = express();
 
-
-
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-// const listing=require("./models/listing.js");
+const mongoose = require("mongoose");
 const path = require("path");
-const methodOverride=require("method-override")
-const ejsMate=require("ejs-mate");
-// const wrapAsync=require("./utils/wrapAsync.js");
-const ExpressError=require("./utils/ExpressError.js");
-// const { render } = require("ejs");
-// const{ listingSchema,reviewSchema }=require("./schema.js");
-// const Review=require("./models/review.js");
-const listingsroutes=require("./routes/listings.js")
-const reviewsroutes=require("./routes/reviews.js")
-const usersroutes=require("./routes/users.js")
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
-// const session=require("express-session");
-//  const MongoStore=require('connect-mongo')
+const ExpressError = require("./utils/ExpressError.js");
+
+const listingsroutes = require("./routes/listings.js");
+const reviewsroutes = require("./routes/reviews.js");
+const usersroutes = require("./routes/users.js");
 
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
+const flash = require("connect-flash");
 
-
-const flash=require("connect-flash");
-
-const passport=require("passport");
+const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
-const User=require("./models/user.js");
+const User = require("./models/user.js");
 
 
+// MongoDB URL
+const dburl = process.env.ATLASDB_URL;
 
 
-
-
-// mongodb://127.0.0.1:27017/wanderlust"
- const dburl=process.env.ATLASDB_URL;
-
-
-main().then(()=>{
-    console.log("connected to DB")
-
-}).catch((err)=>{
-    console.log(err);
-});
-
-
+// Database connection
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main(){
-    await mongoose.connect(dburl)
+    await mongoose.connect(dburl);
 }
 
-app.engine( "ejs",ejsMate);
 
+// EJS Mate setup
+app.engine("ejs", ejsMate);
+
+
+// View engine setup
 app.set("view engine", "ejs");
+
+
+// Views folder path
 app.set("views", path.join(__dirname, "views"));
 
 
-app.use(express.urlencoded({extended:true}));
+// Form data read karne ke liye
+app.use(express.urlencoded({extended: true}));
+
+
+// PUT aur DELETE request ke liye
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname,"/public")))
-
-// const store=MongoStore.create({
-//     mongoUrl:dburl,
-//     crypto:{
-//         secret:process.env.SECRET,
-//     },
-//     touchAfter:24*3600,
-// });
 
 
-// // store.on("error",()=>{
-// //     console.log("ERROR IN MONGO SESSION STORE",err);
-// // });
+// Static files ke liye
+app.use(express.static(path.join(__dirname, "/public")));
 
 
+// MongoDB session store
 const store = MongoStore.create({
     mongoUrl: process.env.ATLASDB_URL,
     touchAfter: 24 * 3600
 });
 
+
+// Mongo session error handle
 store.on("error", (err) => {
     console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
+
+// Session options
 const sessionOptions = {
     store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
+
     cookie: {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 3,
         maxAge: 1000 * 60 * 60 * 24 * 3,
@@ -104,80 +97,82 @@ const sessionOptions = {
 };
 
 
-// const sessionOptions={
-//      store,
-//     secret:process.env.SECRET,
-//     resave:false,
-//     saveUninitialized:true
-// };
-
-
-
-
-// app.use(session(sessionOptions));
-
-
+// Session middleware
 app.use(session(sessionOptions));
+
+
+// Flash middleware
 app.use(flash());
 
 
+// Passport initialize
 app.use(passport.initialize());
+
+
+// Passport session
 app.use(passport.session());
+
+
+// Local Strategy
 passport.use(new LocalStrategy(User.authenticate()));
+
+
+// User serialize
 passport.serializeUser(User.serializeUser());
+
+
+// User deserialize
 passport.deserializeUser(User.deserializeUser());
 
 
-app.use((req,res,next)=>{
-    res.locals.success=req.flash("success");
-    res.locals.error=req.flash("error");
-    //console.log(req.locals.success); // ye ek array h to jab iski lenght  nhi hogi  aur success bhi nhi hoga to alert ka div  nhi dikhega  sabhi page per 
-    res.locals.currUser=req.user;
+// IMPORTANT MIDDLEWARE
+app.use((req, res, next) => {
+
+    console.log("Middleware chal raha hai");
+    console.log("User:", req.user);
+
+    res.locals.success = req.flash("success");
+
+    res.locals.error = req.flash("error");
+
+    res.locals.currUser = req.user || null;
+
     next();
 });
 
-//Demo User
-// app.get("/demouser",async(req,res)=>{
-//     let fakeUser=new User({
-//         email:"student@gmail.com",
-//         username:"delta-student",
-//     });
 
-//     let registerUser=await User.register(fakeUser,"Helloworld");
-//     res.send(registerUser);
-// })
-
-
-
-app.get("/",(req,res)=>{
-   return res.redirect("/listings");
-});
-
-app.use("/listings",listingsroutes);
-app.use("/listings/:id/reviews",reviewsroutes);
-app.use("/",usersroutes);
-
-
-
-app.use((req,res,next)=>{
-    next(new ExpressError(404 ,"Page Not Found!"))
+// Home route
+app.get("/", (req, res) => {
+    return res.redirect("/listings");
 });
 
 
-
-//Error Handling Middleware
-// app.use((err,req,res,next)=>{
-//     let{statusCode=500,message="Something went wrong!"}=err;
-//   return res.status(statusCode).render("error.ejs",{err});
-//     // res.status(statusCode).send(message);
-
-// })
+// Listings routes
+app.use("/listings", listingsroutes);
 
 
+// Reviews routes
+app.use("/listings/:id/reviews", reviewsroutes);
 
+
+// User routes
+app.use("/", usersroutes);
+
+
+// 404 Error
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
+
+// Error Handling Middleware
 app.use((err, req, res, next) => {
+
     let { statusCode = 500 } = err;
-    if (!err.message) err.message = "Something went wrong!";
+
+    if (!err.message) {
+        err.message = "Something went wrong!";
+    }
 
     if (res.headersSent) {
         return next(err);
@@ -187,10 +182,9 @@ app.use((err, req, res, next) => {
 });
 
 
+// Server start
+const port = process.env.PORT || 8080;
 
-app.listen(8080,()=>{
-    console.log("server is listening to port 8080")
+app.listen(port, () => {
+    console.log("server is listening to port " + port);
 });
-
-
-
